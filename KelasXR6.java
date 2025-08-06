2f885f72849d68f21721ef180ef6d83a042c1f45
fahmi12musyaffa <muhammad_musyaffa_34rpl@student.smktelkom-mlg.sch.id>
2025-08-06 11:36:12 +0700

    Add files via upload

---
+++ KelasXR6.java
@@ -0,0 +1,10 @@
+public class KelasXR6 {
+    public static void main(String[] args) {
+      System.out.println("nama: M.Fahmi Musyaffa");
+      System.out.println("Asal sekolah : SMPN 25 Malang");
+      System.out.println("Alamat Rumah : Pondok Bestari Indah E6/231");
+      System.out.println("Jenis Kelamin : Laki-laki");
+      System.out.println("Cita-cita : Web developer");
+      System.out.println("Hobi : Vidiografer");
+    }
+}
